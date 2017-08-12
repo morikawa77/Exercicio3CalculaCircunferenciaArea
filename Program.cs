aace2830eff5fcebb714bb09276aacbe94ae309d
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-12 04:15:20 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio3CalculaCircunferenciaArea
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+			int raio;
+			double circ, area, raio2;
+			Console.WriteLine("Digite o raio do círculo: ");
+			raio = Convert.ToInt32(Console.ReadLine());
+            raio2 = raio * raio;
+			circ = 2 * Math.PI * raio;
+            area = Math.PI * raio2;
+			Console.Write("A área do Círculo é " + area + ", e sua circunferência é " + circ);
+			Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Exercício 3 - Calcular circunferência e área de círculo C# Curso Programação de Dispositivos Móveis SENAC
