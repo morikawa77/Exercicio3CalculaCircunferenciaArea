66535f24006073a6222fbd5f30c1337959540805
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-12 05:01:43 -0300

    Usando menos variáveis e implementando funções de potência

--- Program.cs
+++ Program.cs
@@ -6,13 +6,11 @@ namespace Exercicio3CalculaCircunferenciaArea
     {
         public static void Main(string[] args)
         {
-			int raio;
-			double circ, area, raio2;
+			double circ, area, raio;
 			Console.WriteLine("Digite o raio do círculo: ");
-			raio = Convert.ToInt32(Console.ReadLine());
-            raio2 = raio * raio;
-			circ = 2 * Math.PI * raio;
-            area = Math.PI * raio2;
+            raio = Convert.ToDouble(Console.ReadLine());
+            circ = 2 * Math.PI * raio;
+            area = Math.PI * Math.Pow(raio, 2);
 			Console.Write("A área do Círculo é " + area + ", e sua circunferência é " + circ);
 			Console.ReadLine();
         }
